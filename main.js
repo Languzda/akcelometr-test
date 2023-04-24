293595084894d5ffb9928f71a898cc25ffab7bce
Languzda <langusda@gmail.com>
2023-04-24 12:23:53 +0200

    test 3

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
 import "./style.css";
 
 const app = document.querySelector("#app");
+const btn = document.querySelector("#btn");
 
-if (window.DeviceMotionEvent) {
+if (window.DeviceMotionEvent && window.DeviceOrientationEvent) {
   window.addEventListener("devicemotion", function (event) {
-    console.log(event.acceleration.x + " m/s2");
-    app.innerHTML = `<h1>${event.acceleration.x} m/s2</h1>`;
+    console.log(
+      "Acceleration: " +
+        event.acceleration.x +
+        ", " +
+        event.acceleration.y +
+        ", " +
+        event.acceleration.z +
+        " m/s2"
+    );
+    app.innerHTML = `<h1>Acceleration: ${event.acceleration.x}, ${event.acceleration.y}, ${event.acceleration.z} m/s2</h1>`;
+  });
+
+  window.addEventListener("deviceorientation", function (event) {
+    console.log(
+      "Orientation: " +
+        event.alpha +
+        ", " +
+        event.beta +
+        ", " +
+        event.gamma +
+        " deg"
+    );
+    app.innerHTML += `<h1>Orientation: ${event.alpha}, ${event.beta}, ${event.gamma} deg</h1>`;
   });
 }
-setupCounter(document.querySelector("#counter"));
+
+btn.addEventListener("click", () => {
+  if ("vibrate" in navigator) {
+    navigator.vibrate(1000); // czas trwania wibracji w milisekundach
+  }
+});
