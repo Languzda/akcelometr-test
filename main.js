8cd034bd4fe501f5236b42ef6d6307a98798c097
Languzda <langusda@gmail.com>
2023-04-26 08:40:13 +0200

    test 3

--- main.js
+++ main.js
@@ -6,33 +6,41 @@ const btn = document.querySelector("#btn");
 const acc = document.querySelector("#acc");
 const ori = document.querySelector("#ori");
 
-if (window.DeviceMotionEvent && window.DeviceOrientationEvent) {
-  window.addEventListener("devicemotion", function (event) {
-    console.log(
-      "Acceleration: " +
-        event.acceleration.x +
-        ", " +
-        event.acceleration.y +
-        ", " +
-        event.acceleration.z +
-        " m/s2"
-    );
-    acc.innerHTML = ` ${event.acceleration.x}, ${event.acceleration.y}, ${event.acceleration.z} m/s2`;
-  });
+setInterval(() => {
+  if (window.DeviceMotionEvent && window.DeviceOrientationEvent) {
+    window.addEventListener("devicemotion", function (event) {
+      console.log(
+        "Acceleration: " +
+          event.acceleration.x +
+          ", " +
+          event.acceleration.y +
+          ", " +
+          event.acceleration.z +
+          " m/s2"
+      );
+      acc.innerHTML = ` ${event.acceleration.x.toFixed(
+        2
+      )} X, ${event.acceleration.y.toFixed(
+        2
+      )} Y, ${event.acceleration.z.toFixed(2)} Z m/s2`;
+    });
 
-  window.addEventListener("deviceorientation", function (event) {
-    console.log(
-      "Orientation: " +
-        event.alpha +
-        ", " +
-        event.beta +
-        ", " +
-        event.gamma +
-        " deg"
-    );
-    ori.innerHTML = ` ${event.alpha}, ${event.beta}, ${event.gamma} deg`;
-  });
-}
+    window.addEventListener("deviceorientation", function (event) {
+      console.log(
+        "Orientation: " +
+          event.alpha +
+          ", " +
+          event.beta +
+          ", " +
+          event.gamma +
+          " deg"
+      );
+      ori.innerHTML = ` ${event.alpha.toFixed(2)} alpha, ${event.beta.toFixed(
+        2
+      )} beta, ${event.gamma.toFixed(2)} gamma deg`;
+    });
+  }
+}, 150);
 
 btn.addEventListener("click", () => {
   if ("vibrate" in navigator) {
